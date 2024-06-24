22274e6ca636ec47efafe0757734ebfdcedf986a
Dmitriy Dyomin <demin.da@mipt.ru>
2024-06-24 12:44:04 +0300

    WIP

--- feko/dump_parameters.lua
+++ feko/dump_parameters.lua
@@ -3,6 +3,7 @@ project = app.Project
 vars = project.Variables
 for i = 1,#vars do
     v = vars[i]
-    print(string.format("%s, \"%s\", %.6f, \"%s\"", 
-        v.Name, v.Expression, v.Value, v.Description));
+    --print(string.format("%s, \"%s\", %.6f, \"%s\"", 
+    --    v.Name, v.Expression, v.Value, v.Description));
+    print(string.format("\"%s\" : %.6f,", v.Name, v.Value));
 end
